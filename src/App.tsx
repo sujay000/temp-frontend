fb54b63b7485838a32b5f3bc68368c3287229f36
sujay sapkal <sujsap@amazon.com>
2024-08-31 01:14:21 +0530

    try links in code

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import Home from './Home'
 
 function App() {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<div>Home</div>} />
+        <Route path='/' element={<Home />} />
         <Route path='/about' element={<div>About</div>} />
         <Route path='*' element={<div>Not Found</div>} />
       </Routes>
